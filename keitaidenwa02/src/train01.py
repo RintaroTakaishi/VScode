7b1ecf34e8980377accbf2e18502d427aef86dad
TAKAISHI <taka100_1224@yahoo.co.jp>
2024-05-27 11:30:25 +0900

    atarasiku

--- keitaidenwa02/src/train01.py
+++ keitaidenwa02/src/train01.py
@@ -1,1 +1,7 @@
 import pandas as pd
+import numpy as np
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridsearchCV, StartifieldKfold, train_test_sprit
+from sklearn.svm import SVC
+
